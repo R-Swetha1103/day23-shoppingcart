e41c172631bf7c621571d21022864af0a136d7c5
R-Swetha1103 <swetharagupathy1103@gmail.com>
2024-08-17 22:27:07 +0530

    react 23

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,105 @@
 
-import React from 'react'
-import Card from './card.jsx'
+import React,{useState} from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import TopBar from "./components/TopBar";
 function App() {
-  let data= [
+  let [cart, setCart] = useState(0);
+  let data =[
     {
-      plan:"FREE",
-      price:0,
-      isUser:true,
-      user:"Single User",
-      isStorage:true,
-      storage:"5 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:false,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:false,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:false,
-      subDomain:"Free Subdomain",
-      isReports:false,
-      reports:"Monthly Status Reports"
+      isSale:false,
+      sale:"sale",
+      productName:"Fancy Product",
+      isPrice1:false,
+      price1:"$40.00 -",
+      isPrice2:true,
+      price2:" $80.00",
+      
     },
     {
-      plan:"PLUS",
-      price:9,
-      isUser:true,
-      user:"5 Users",
-      isStorage:true,
-      storage:"50 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:true,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:true,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:true,
-      subDomain:"Free Subdomain",
-      isReports:false,
-      reports:"Monthly Status Reports"
+      isSale:true,
+      sale:"sale",
+      productName:"Special Item",
+      isPrice1:true,
+      price1:"$20.00",
+      isPrice2:true,
+      price2:" $18.00",
     },
     {
-      plan:"PRO",
-      price:49,
-      isUser:true,
-      user:"Unlimited Users",
-      isStorage:true,
-      storage:"150 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:true,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:true,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:true,
-      subDomain:"Free Subdomain",
-      isReports:true,
-      reports:"Monthly Status Reports"
+      isSale:true,
+      sale:"sale",
+      productName:"Sale Item",
+      isPrice1:true,
+      price1:"$50.00",
+      isPrice2:true,
+      price2:" $25.00",
+    },
+    {
+      isSale:false,
+      sale:"sale",
+      productName:"Popular Item",
+      isPrice1:true,
+      price1:null,
+      isPrice2:true,
+      price2:" $40.00",
+    },
+    {
+      isSale:true,
+      sale:"sale",
+      productName:"Sale Item",
+      isPrice1:true,
+      price1:"$50.00",
+      isPrice2:true,
+      price2:" $25.00",
+    },
+    {
+      isSale:false,
+      sale:"sale",
+      productName:"Fancy Product",
+      isPrice1:false,
+      price1:"$120.00 -",
+      isPrice2:true,
+      price2:" $280.00",
+    },
+    {
+      isSale:true,
+      sale:"sale",
+      productName:"Special Item",
+      isPrice1:true,
+      price1:"$20.00",
+      isPrice2:true,
+      price2:" $18.00",
+    },
+    {
+      isSale:false,
+      sale:"sale",
+      productName:"Popular Item",
+      isPrice1:true,
+      price1:null,
+      isPrice2:true,
+      price2:" $40.00",
     }
   ]
-  return <section className="pricing py-5">
-  <div className="container">
-    <div className="row">
-      {
+  return <>
+  
+      <TopBar cart={cart} setCart={setCart}/>
+      <Header />
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+          {
         data.map((e,i)=>{
-          return  <Card data={e} key={i} />
+          return  <Card cart={cart} setCart={setCart} data={e} key={i}/> 
         })
       }
-     
-      
-      
-    </div>
-  </div>
-</section>
+            
+          </div>
+        </div>
+      </section>
+    <footer className="py-5 bg-dark">
+    <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+</footer>
+</>
 }
 
-export default App
+export default App;
